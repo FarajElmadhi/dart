90ccdfc28c5bae71426f75fb1f2e295a2ee4106c
FARAJ ELMADHI <63078234+FarajElmadhi@users.noreply.github.com>
2021-10-01 11:52:35 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,17 @@
+//=== | Variable   :   String, int, double,bool, var, List, Map, Set, final, const  
+//  ---final const
+// === | if , switch , if shortcut
+// === | for Loop | -------------
+// === | Numbers |  :  isEven , isOdd, isInfinite => true or false
+//                           :  ceil(), round(), floor(), abs(), ceilToDouble()
+
+// === | String  :  isEmpty , length, toUpperCase ..., trim(), toSet()
+//                             comparTo('string'),split(), replaceAll(from, to)
+
+// === | List :  add(), first, last, isEmpty, isNotEmpty, reversed.toList(), single, addAll
+//                         insert(index, value), insertAll(index, val), replaceRange(), removeAt(), removeRange()
+
+// === | Map : length, keys, value
+// === | function : void , return
+
+// === import, import as
