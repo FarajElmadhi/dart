6577c9cab25b2b073babea6d41499a9959cd2c93
FARAJ ELMADHI <63078234+FarajElmadhi@users.noreply.github.com>
2021-10-11 19:36:52 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,25 +1,6 @@
-//=== | Variable   :   String, int, double,bool, var, List, Map, Set, final, const  
-//  ---final const
-// === | if , switch , if shortcut
-// === | for Loop | -------------
-// === | Numbers |  :  isEven , isOdd, isInfinite => true or false
-//                           :  ceil(), round(), floor(), abs(), ceilToDouble()
+  baseUrl = "https://android4ever.com/sms/sms/public/api";
+// Login 
+//  baseUrl/login,  post :[String email, String password], Failed login : error, success:  token
 
-// === | String  :  isEmpty , length, toUpperCase ..., trim(), toSet()
-//                             comparTo('string'),split(), replaceAll(from, to)
-
-// === | List :  add(), first, last, isEmpty, isNotEmpty, reversed.toList(), single, addAll
-//                         insert(index, value), insertAll(index, val), replaceRange(), removeAt(), removeRange()
-
-// === | Map : length, keys, value
-// === | function : void , return
-
-// === import, import as
-
-
-
-// ========OOP :
-//   ---- class
-//  ------ function=>methodes
-// ---------- constructors , short constructor (this.any)
-// RESET PARAMETRES CLASS : CASCADE operator ..
+  // Register 
+//  baseUrl/login,  post :[String name, String email, String password, String c_password], Failed register  : [error][param] , success:  token
