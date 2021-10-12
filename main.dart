a1735237215008b2db8a0f362b9b637077702069
FARAJ ELMADHI <63078234+FarajElmadhi@users.noreply.github.com>
2021-10-12 18:55:59 +0300

    Delete main.dart

--- main.dart
+++
@@ -1,6 +0,0 @@
-  baseUrl = "https://android4ever.com/sms/sms/public/api";
-// Login 
-//  baseUrl/login,  post :[String email, String password], Failed login : error, success:  token
-
-  // Register 
-//  baseUrl/login,  post :[String name, String email, String password, String c_password], Failed register  : [error][param] , success:  token
